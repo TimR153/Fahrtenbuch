8766320ed8a6b9359599824ed6674045ae5df055
Tim Rentrop <timrentrop@proton.me>
2025-10-19 13:01:48 +0200

    Client Generierung mit NSwag hinzugefügt.

--- Fahrtenbuch.Web.Api/Program.cs
+++ Fahrtenbuch.Web.Api/Program.cs
@@ -1,14 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "v1";
+    config.Title = "Fahrtenbuch.Web.Api";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
